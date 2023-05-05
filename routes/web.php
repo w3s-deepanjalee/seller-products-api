b65e804f213b23433a7372b59eb084c7eb3c7b85
w3sdeepanjalee <deepanjalee@w3ssolutions.com>
2023-05-05 08:42:50 +0530

    Api Document

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::middleware([
-    'auth:sanctum',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
+// Route::middleware([
+//     'auth:sanctum',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
 
-    })->name('dashboard');
+//     })->name('dashboard');
 
-    Route::prefix('admin')->group(function () {
+//     Route::prefix('admin')->group(function () {
 
-    });
-});
+//     });
+// });
 
-Route::resource('admin/products', ProductController::class);
+// Route::resource('admin/products', ProductController::class);
